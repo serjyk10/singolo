b151df33600593418dfbf8a3c1ea93fc459215c7
Sergey Borisevich <borisevich.serjyk@gmail.com>
2020-03-16 12:11:13 +0300

    feat: contacts part is solved and fixed header bugs

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const SLIDER_WRAP = document.querySelector(".slider-phone");
 const SLIDER = document.querySelector(".slider-content");
 const BUTTON_PREV = document.querySelector(".button_prev");
 const BUTTON_NEXT = document.querySelector(".button_next");
-const PHONE_BUTTON_VERT = document.querySelector(".slider-phone__vertical .phone_button");
-const PHONE_BUTTON_GORIZ = document.querySelector(".slider-phone__gorizontal .phone_button");
-const SCREEN_VERTICAL = document.querySelector(".slider-phone__vertical .screen-off");
-const SCREEN_GORIZONTAL = document.querySelector(".slider-phone__gorizontal .screen-off");
+
+const PHONE_BUTTON_VERT = document.querySelector(
+  ".slider-phone__vertical .phone_button"
+);
+const PHONE_BUTTON_GORIZ = document.querySelector(
+  ".slider-phone__gorizontal .phone_button"
+);
+const SCREEN_VERTICAL = document.querySelector(
+  ".slider-phone__vertical .screen-off"
+);
+const SCREEN_GORIZONTAL = document.querySelector(
+  ".slider-phone__gorizontal .screen-off"
+);
 let SCREEN_STATUS_VERT = true;
 let SCREEN_STATUS_GOR = true;
 
@@ -21,127 +30,152 @@ const BTN_PORTFOLIO_ART = document.querySelector(".tag_art");
 const PORTFOLIO_ITEMS = document.querySelectorAll(".portfolio-works__item");
 const PORTFOLIO_BLOCK = document.querySelector(".portfolio-works");
 
-
+const CONTACTS_BTN = document.querySelector(".contacts-form__button");
+const MODAL = document.querySelector(".contacts-modal");
+const MODAL_BTN = document.querySelector(".modal-button");
+const FORM = document.querySelector("form");
 
 MENU.addEventListener("click", event => {
-    MENU.querySelectorAll("li").forEach(el => {
-        el.querySelector("a").classList.remove("active_item");
-        event.target.classList.add("active_item");
-    });
+  MENU.querySelectorAll("li").forEach(el => {
+    el.querySelector("a").classList.remove("active_item");
+    event.target.classList.add("active_item");
+  });
 });
 
 BUTTON_PREV.addEventListener("click", event => {
-    SLIDES.forEach(el => {
-        el.style.display = "none";
-        SLIDER_WRAP.classList.remove("bg-blue");
-        SLIDER.classList.remove("slider-resize");
-    });
-    console.log(SLIDE_NUM);
-    SLIDE_NUM - 1 < 0 ? SLIDE_NUM++ : SLIDE_NUM--;
+  SLIDES.forEach(el => {
+    el.style.display = "none";
+    SLIDER_WRAP.classList.remove("bg-blue");
+    SLIDER.classList.remove("slider-resize");
+  });
+  console.log(SLIDE_NUM);
+  SLIDE_NUM - 1 < 0 ? SLIDE_NUM++ : SLIDE_NUM--;
 
-    if (SLIDES[SLIDE_NUM].classList.contains("slider-modify")) {
-        SLIDER_WRAP.classList.add("bg-blue");
-        SLIDER.classList.add("slider-resize");
-    }
+  if (SLIDES[SLIDE_NUM].classList.contains("slider-modify")) {
+    SLIDER_WRAP.classList.add("bg-blue");
+    SLIDER.classList.add("slider-resize");
+  }
 
-    SLIDES[SLIDE_NUM].style.display = "flex";
+  SLIDES[SLIDE_NUM].style.display = "flex";
 });
 
 BUTTON_NEXT.addEventListener("click", event => {
-    SLIDES.forEach(el => {
-        el.style.display = "none";
-        SLIDER_WRAP.classList.remove("bg-blue");
-        SLIDER.classList.remove("slider-resize");
+  SLIDES.forEach(el => {
+    el.style.display = "none";
+    SLIDER_WRAP.classList.remove("bg-blue");
+    SLIDER.classList.remove("slider-resize");
+  });
+  console.log(SLIDE_NUM);
+  SLIDE_NUM + 1 == SLIDES.length ? (SLIDE_NUM = 0) : SLIDE_NUM++;
+  console.log(SLIDE_NUM);
 
-    });
-    console.log(SLIDE_NUM);
-    SLIDE_NUM + 1 == SLIDES.length ? SLIDE_NUM = 0 : SLIDE_NUM++;
-    console.log(SLIDE_NUM);
+  if (SLIDES[SLIDE_NUM].classList.contains("slider-modify")) {
+    SLIDER_WRAP.classList.add("bg-blue");
+    SLIDER.classList.add("slider-resize");
+  }
 
-    if (SLIDES[SLIDE_NUM].classList.contains("slider-modify")) {
-        SLIDER_WRAP.classList.add("bg-blue");
-        SLIDER.classList.add("slider-resize");
-    }
-
-    SLIDES[SLIDE_NUM].style.display = "flex";
-
+  SLIDES[SLIDE_NUM].style.display = "flex";
 });
 
 PHONE_BUTTON_VERT.addEventListener("click", event => {
-    if (SCREEN_STATUS_VERT) {
-        console.log(SCREEN_VERTICAL);
-        SCREEN_VERTICAL.classList.add("display-off");
-        SCREEN_STATUS_VERT = false;
-    } else if (!SCREEN_STATUS_VERT) {
-        SCREEN_VERTICAL.classList.remove("display-off");
-        SCREEN_STATUS_VERT = true;
-    }
+  if (SCREEN_STATUS_VERT) {
+    console.log(SCREEN_VERTICAL);
+    SCREEN_VERTICAL.classList.add("display-off");
+    SCREEN_STATUS_VERT = false;
+  } else if (!SCREEN_STATUS_VERT) {
+    SCREEN_VERTICAL.classList.remove("display-off");
+    SCREEN_STATUS_VERT = true;
+  }
 });
 
-
 PHONE_BUTTON_GORIZ.addEventListener("click", event => {
-    if (SCREEN_STATUS_GOR) {
-        console.log(SCREEN_GORIZONTAL);
-        SCREEN_GORIZONTAL.classList.add("display-off");
-        SCREEN_STATUS_GOR = false;
-    } else if (!SCREEN_STATUS_GOR) {
-        SCREEN_GORIZONTAL.classList.remove("display-off");
-        SCREEN_STATUS_GOR = true;
-    }
+  if (SCREEN_STATUS_GOR) {
+    console.log(SCREEN_GORIZONTAL);
+    SCREEN_GORIZONTAL.classList.add("display-off");
+    SCREEN_STATUS_GOR = false;
+  } else if (!SCREEN_STATUS_GOR) {
+    SCREEN_GORIZONTAL.classList.remove("display-off");
+    SCREEN_STATUS_GOR = true;
+  }
 });
 
 BTN_PORTFOLIO_ALL.addEventListener("click", event => {
-    TAGS.forEach(el => {
-        el.classList.remove("active-tag");
-    });
-    BTN_PORTFOLIO_ALL.classList.add("active-tag");
-    PORTFOLIO_ITEMS[0].classList.remove("item-first");
-    PORTFOLIO_ITEMS[1].classList.remove("item-first");
-    PORTFOLIO_ITEMS[2].classList.remove("item-first");
-    PORTFOLIO_ITEMS[3].classList.remove("item-first");
+  TAGS.forEach(el => {
+    el.classList.remove("active-tag");
+  });
+  BTN_PORTFOLIO_ALL.classList.add("active-tag");
+  PORTFOLIO_ITEMS[0].classList.remove("item-first");
+  PORTFOLIO_ITEMS[1].classList.remove("item-first");
+  PORTFOLIO_ITEMS[2].classList.remove("item-first");
+  PORTFOLIO_ITEMS[3].classList.remove("item-first");
 });
 
 BTN_PORTFOLIO_WB.addEventListener("click", event => {
-    TAGS.forEach(el => {
-        el.classList.remove("active-tag");
-    });
-    BTN_PORTFOLIO_WB.classList.add("active-tag");
-    PORTFOLIO_ITEMS[0].classList.remove("item-first");
-    PORTFOLIO_ITEMS[1].classList.remove("item-first");
-    PORTFOLIO_ITEMS[2].classList.remove("item-first");
-    PORTFOLIO_ITEMS[3].classList.remove("item-first");
-    PORTFOLIO_ITEMS[0].classList.add("item-first");
+  TAGS.forEach(el => {
+    el.classList.remove("active-tag");
+  });
+  BTN_PORTFOLIO_WB.classList.add("active-tag");
+  PORTFOLIO_ITEMS[0].classList.remove("item-first");
+  PORTFOLIO_ITEMS[1].classList.remove("item-first");
+  PORTFOLIO_ITEMS[2].classList.remove("item-first");
+  PORTFOLIO_ITEMS[3].classList.remove("item-first");
+  PORTFOLIO_ITEMS[0].classList.add("item-first");
 });
 
 BTN_PORTFOLIO_GD.addEventListener("click", event => {
-    TAGS.forEach(el => {
-        el.classList.remove("active-tag");
-    });
-    BTN_PORTFOLIO_GD.classList.add("active-tag");
-    PORTFOLIO_ITEMS[0].classList.remove("item-first");
-    PORTFOLIO_ITEMS[1].classList.remove("item-first");
-    PORTFOLIO_ITEMS[2].classList.remove("item-first");
-    PORTFOLIO_ITEMS[3].classList.remove("item-first");
-    PORTFOLIO_ITEMS[0].classList.add("item-first");
-    PORTFOLIO_ITEMS[1].classList.add("item-first");
+  TAGS.forEach(el => {
+    el.classList.remove("active-tag");
+  });
+  BTN_PORTFOLIO_GD.classList.add("active-tag");
+  PORTFOLIO_ITEMS[0].classList.remove("item-first");
+  PORTFOLIO_ITEMS[1].classList.remove("item-first");
+  PORTFOLIO_ITEMS[2].classList.remove("item-first");
+  PORTFOLIO_ITEMS[3].classList.remove("item-first");
+  PORTFOLIO_ITEMS[0].classList.add("item-first");
+  PORTFOLIO_ITEMS[1].classList.add("item-first");
 });
 
 BTN_PORTFOLIO_ART.addEventListener("click", event => {
-    TAGS.forEach(el => {
-        el.classList.remove("active-tag");
-    });
-    BTN_PORTFOLIO_ART.classList.add("active-tag");
-    PORTFOLIO_ITEMS[0].classList.add("item-first");
-    PORTFOLIO_ITEMS[1].classList.add("item-first");
-    PORTFOLIO_ITEMS[2].classList.add("item-first");
-    PORTFOLIO_ITEMS[3].classList.add("item-first");
+  TAGS.forEach(el => {
+    el.classList.remove("active-tag");
+  });
+  BTN_PORTFOLIO_ART.classList.add("active-tag");
+  PORTFOLIO_ITEMS[0].classList.add("item-first");
+  PORTFOLIO_ITEMS[1].classList.add("item-first");
+  PORTFOLIO_ITEMS[2].classList.add("item-first");
+  PORTFOLIO_ITEMS[3].classList.add("item-first");
 });
-
 
 PORTFOLIO_BLOCK.addEventListener("click", event => {
-    PORTFOLIO_ITEMS.forEach(el => {
-        el.querySelector("img").classList.remove("border-item");
-        event.target.classList.add("border-item");
-    });
+  PORTFOLIO_ITEMS.forEach(el => {
+    el.querySelector("img").classList.remove("border-item");
+    event.target.classList.add("border-item");
+  });
 });
 
+FORM.addEventListener("submit", event => {
+  event.preventDefault();
+});
+
+CONTACTS_BTN.addEventListener("click", event => {
+  let name = document.querySelector(".contacts-form__name");
+  let email = document.querySelector(".contacts-form__email");
+
+  if (name.checkValidity() && email.checkValidity()) {
+    MODAL.classList.add("modal-on");
+    let subject = document.querySelector(".contacts-form__subject").value;
+    subject = subject ? subject : "Subject is empty";
+    let describe = document.querySelector(".contacts-form__textarea").value;
+    describe = describe ? describe : "Describe is empty";
+
+    document.querySelector(".contacts-modal__subject").textContent =
+      "Subject:" + " " + subject;
+    document.querySelector(".contacts-modal__describe").textContent =
+      "Description:" + " " + describe;
+  }
+});
+
+MODAL_BTN.addEventListener("click", event => {
+  MODAL.classList.remove("modal-on");
+  FORM.reset();
+});
